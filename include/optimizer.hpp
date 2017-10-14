be592e2d395f9cfcb64da1aebf0a31583fed86e8
ShaotuJia <shaotu.jia@hotmail.com>
2017-10-13 23:08:55 -0400

    save current work and change computer

---
+++ include/optimizer.hpp
@@ -0,0 +1,29 @@
+/**
+ * @breif: This is the header file for optimizer class which
+ * applies Simuluated Annealing
+ * @author: Shaotu Jia
+ * @copyright:
+ */
+
+ #ifndef OPTIMIZER_HPP
+ #define OPTIMIZER_HPP
+
+class Optimizer {
+ private:
+     double step = 0.01;
+     
+
+ public:
+
+
+
+
+};
+
+
+
+
+
+
+
+ #endif
